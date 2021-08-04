00aa099f552e7f84841e1ce27ad8d4295ca4718c
Lorcan Nicholls <ln356@cam.ac.uk>
2021-08-04 21:28:36 +0100

    Optimised build

--- Build/build.bat
+++ Build/build.bat
@@ -3,11 +3,13 @@ cd Build
 :: ensure pyinstaller is updated in order to use the (currently experimental) --splash flag
 pip install --upgrade pyinstaller
 
-pyinstaller ../Source/Truss_Calculator.py                                                   ^
-            --onefile                                                                       ^
-            --splash ../Media/TrussBridge.jpg                                               ^
-            --icon ../Media/TrussIcon.ico                                                   ^
-            --upx-dir C:\Users\lnick\Downloads\upx-3.96-win64\upx-3.96-win64                ^
-            --noconsole
+:: build with basic optimisations
+python -O -m PyInstaller                                                                    ^
+    ../Source/Truss_Calculator.py                                                           ^
+    --onefile                                                                               ^
+    --splash ../Media/TrussBridge.jpg                                                       ^
+    --icon ../Media/TrussIcon.ico                                                           ^  
+    --upx-dir C:\Users\lnick\Downloads\upx-3.96-win64\upx-3.96-win64                        ^
+    --noconsole
 
 echo " --- If the build completed succesfully, it can be found in the `dist` folder. --- "
