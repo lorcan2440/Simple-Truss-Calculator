d90a75549f7bcf4145693fbdb9438a356362ca06
Lorcan Nicholls <ln356@cam.ac.uk>
2023-01-31 15:23:00 +0800

    Added CI/CD

--- Examples/_import_helper.py
+++
@@ -1,6 +0,0 @@
-from os.path import *
-import os, sys
-
-PACKAGE_PARENT = '..'
-SCRIPT_DIR = dirname(realpath(join(os.getcwd(), expanduser(__file__))))
-sys.path.append(normpath(join(SCRIPT_DIR, PACKAGE_PARENT)))
